7f9774eb8bd0e94f05332f2c2ac33fa1048e87c3
Ubuntu <ubuntu@ip-172-31-19-120.us-east-2.compute.internal>
2024-04-25 04:24:12 +0000

    okay

--- db/migrate/20240425020642_create_admin_posts.rb
+++ db/migrate/20240425020642_create_admin_posts.rb
@@ -1,6 +1,6 @@
 class CreateAdminPosts < ActiveRecord::Migration[7.1]
   def change
-    create_table :admin_posts do |t|
+    create_table :posts do |t|
       t.string :name
       t.string :title
       t.text :content
